d4ebe957618a7401013108fd79c4a23dde264e83
baskorokers21 <60204813+baskorokers21@users.noreply.github.com>
2020-07-26 13:15:09 +0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+var organ = ["OSIS","MPK","PUSTEL","Dewan Ambalan"]; //array berindeks
+
+// akses elemen menggunakan indeks 0,1,2 .....
+console.log(organ[0]);
+console.log(organ[1]);
+console.log(organ[2]);
+console.log("\n");
+
+// array assosiatif
+var OSIS = {'Ketua':'a','wakil':'b','sekretaris':'c'};
+
+console.log(OSIS['Ketua']);
+console.log(OSIS['wakil']);
